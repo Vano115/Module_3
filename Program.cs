fde8331c8029750a0eaca186cdc1a7f5c80aa0b0
Иван Вякин <Beas_wow@mail.ru>
2025-01-17 13:28:39 +0300

    Перечисления

--- Program.cs
+++ Program.cs
@@ -15,7 +15,24 @@ namespace Module_3
             short myShoeSize = 44;
             Console.WriteLine("My name is {0},\nMy age is {1},\nMy shoe size is {2}", myName, myAge, myShoeSize);
 
-            Console.ReadKey();
+            DaysOfWeek myFavouriteDay;
+            myFavouriteDay = DaysOfWeek.friday;
+
+            Console.WriteLine(myFavouriteDay);
         }
+
+            enum DaysOfWeek : byte
+            {
+                    monday = 1,
+                    tuesday,
+                    wednesday,
+                    thursday,
+                    friday,
+                    saturday,
+                    sunday
+            }
+
+
     }
 }
+
